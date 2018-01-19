4dc95b8de7d9f8ffb072e426a8eb1c9bce302518
heliang <heliang@duyao001.com>
2018-01-19 19:14:32 +0800

    添加上传动作

---
+++ public/lib/rem.js
@@ -0,0 +1,1 @@
+(function(d,c){var e=d.documentElement,a="onorientationchange" in window?"onorientationchange":"resize",b=function(){var f=e.clientWidth;if(!f){return}if(f>=750){e.style.fontSize="100px"}else{e.style.fontSize=100*(f/750)+"px"}};if(!d.addEventListener){return}c.addEventListener(a,b,false);d.addEventListener("DOMContentLoaded",b,false)})(document,window);
